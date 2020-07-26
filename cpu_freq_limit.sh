78e308ba7d8bb3f65ebbf33ba25f5054eb884794
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-26 13:31:55 +0200

    script: Fix syntax

--- cpu_freq_limit.sh
+++ cpu_freq_limit.sh
@@ -7,27 +7,23 @@ chain=1094400
 nochain=-1
 thermal_dir="/sys/devices/virtual/thermal/thermal_zone11/temp"
 freq_limit_dir="/sys/power/cpufreq_max_limit"
-while true 
- do
-  for i in 1 2 3 4 5
-   do
+while true; do
+  for i in 1 2 3 4 5; do
      temp[i]=$(cat $thermal_dir)
      sleep 1
   done
   total=0
-  for i in 1 2 3 4 5
-   do
+  for i in 1 2 3 4 5; do
      total=$(( total + temp[i] ))
   done
   avg=$(( total / 5 ))
-  if [ $avg -ge 57 ] ; then
+  if [ $avg -ge 57 ]; then
     chmod 755 $freq_limit_dir
     echo $chain > $freq_limit_dir
     sleep 10
-   else
+  else
     chmod 755 $freq_limit_dir
     echo $nochain > $freq_limit_dir
     sleep 10
   fi
-   #
 done
